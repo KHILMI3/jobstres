c04c07840531cf93cbd50dc215d7807443f242ed
KHILMI3 <khilmi333@students.unnes.ac.id>
2025-02-11 19:19:39 +0700

    TTTTTTTTT

--- mobile/javascript/text_position[2].js
+++ mobile/javascript/text_position[2].js
@@ -1,1 +1,1 @@
-﻿positionForPages[2]={page:2,positions:[{word:"",left:1.000437,top:0.990165,width:0.004191,height:0.013113}]};
+﻿positionForPages[2]={page:2,positions:[{word:"",left:1.000437,top:0.990165,width:0.004191,height:0.013113},{word:" ",left:1.242374,top:0.990165,width:0.004191,height:0.013113}]};
